82bc001b0b4bb982cb4d2cefab35515d15e6def4
OliverVittrup <59997483+OliverVittrup@users.noreply.github.com>
2024-02-09 21:56:26 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,120 @@
+//-------Fade tekst ind-------
+window.addEventListener('scroll', function() {
+    const element = document.querySelector('.fade-in');
+    const position = element.getBoundingClientRect().top;
+  
+    // If the section is visible on the screen
+    if (position < window.innerHeight) {
+      element.style.opacity = 1;
+      element.style.visibility = 'visible';
+    }
+  });
+
+  window.addEventListener('scroll', function() {
+    const element = document.querySelector('#projekter');
+    const position = element.getBoundingClientRect().top;
+  
+    // If the section is visible on the screen
+    if (position < window.innerHeight) {
+      element.style.opacity = 1;
+      element.style.visibility = 'visible';
+    }
+  });
+
+  window.addEventListener('scroll', function() {
+    const element = document.querySelector('#mineinteresser');
+    const position = element.getBoundingClientRect().top;
+  
+    // If the section is visible on the screen
+    if (position < window.innerHeight) {
+      element.style.opacity = 1;
+      element.style.visibility = 'visible';
+    }
+  });
+  window.addEventListener('scroll', function() {
+    const element = document.querySelector('#pil');
+    const position = element.getBoundingClientRect().top;
+  
+    // If the section is visible on the screen
+    if (position < window.innerHeight) {
+      element.style.opacity = 1;
+      element.style.visibility = 'visible';
+    }
+  });
+
+  window.addEventListener('scroll', function() {
+    const element = document.querySelector('#kort');
+    const position = element.getBoundingClientRect().top;
+  
+    // If the section is visible on the screen
+    if (position < window.innerHeight) {
+      element.style.opacity = 1;
+      element.style.visibility = 'visible';
+    }
+  });
+
+  window.addEventListener('scroll', function() {
+    const element = document.querySelector('#kortoverskrift');
+    const position = element.getBoundingClientRect().top;
+  
+    // If the section is visible on the screen
+    if (position < window.innerHeight) {
+      element.style.opacity = 1;
+      element.style.visibility = 'visible';
+    }
+  });
+
+  window.addEventListener('scroll', function() {
+    const element = document.querySelector('#kortunderoverskrift');
+    const position = element.getBoundingClientRect().top;
+  
+    // If the section is visible on the screen
+    if (position < window.innerHeight) {
+      element.style.opacity = 1;
+      element.style.visibility = 'visible';
+    }
+  });
+
+//-------Slide billede ind-------
+window.addEventListener('scroll', function() {
+    const element = document.querySelector('.slide-in');
+    const position = element.getBoundingClientRect().top;
+  
+    // If the image is visible on the screen
+    if (position < window.innerHeight) {
+      element.style.transform = 'translateX(0)';
+    }
+  });
+
+  window.addEventListener('scroll', function() {
+    const element = document.querySelector('.projekter');
+    const position = element.getBoundingClientRect().top;
+  
+    // If the image is visible on the screen
+    if (position < window.innerHeight) {
+      element.style.transform = 'translateX(0)';
+    }
+  });
+
+  window.addEventListener('scroll', function() {
+    const element = document.querySelector('.projekter2');
+    const position = element.getBoundingClientRect().top;
+  
+    // If the image is visible on the screen
+    if (position < window.innerHeight) {
+      element.style.transform = 'translateX(0)';
+    }
+  });
+  
+  window.addEventListener('scroll', function() {
+    const element = document.querySelector('#hero4');
+    const position = element.getBoundingClientRect().top;
+  
+    // If the image is visible on the screen
+    if (position < window.innerHeight) {
+      element.style.transform = 'translateX(0)';
+    }
+  });
+
+   //Slideshow
+
